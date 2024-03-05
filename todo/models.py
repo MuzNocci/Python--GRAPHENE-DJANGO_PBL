a87992a460ce197ba5ae6241930d9b4bd7b06c8f
Müller Nocciolli <muller.nocciolli@gmail.com>
2024-03-05 11:28:14 -0300

    Update "schemas.py".

--- todo/models.py
+++ todo/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Todo(models.Model):
+    
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     timestamp = models.DateTimeField(auto_now_add=True)
--- todo/schemas.py
+++ todo/schemas.py
@@ -1,10 +1,21 @@
 import graphene
 from graphene_django import DjangoObjectType
-from todo.models import Todo
+from todo.models import Todo, User
+
+
+
+class UserType(DjangoObjectType):
+
+    class Meta:
+
+        fields = ['username', 'id', 'first_name', 'last_name']
+        model = User
 
 
 
 class TodoType(DjangoObjectType):
+
+    user = graphene.Field(UserType, id=graphene.Int())
 
     class Meta:
 
@@ -15,21 +26,22 @@ class TodoType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
 
-    todo = graphene.Field(TodoType, id=graphene.Int())
+    todo = graphene.Field(TodoType, id=graphene.String())
     todos = graphene.List(TodoType)
+    success = graphene.Boolean()
 
 
     def resolve_todo(root, info, id):
 
         user = info.context.user
-        return Todo.objects.select_related('user').get(id=id, user=user)
-    
+        return Todo.objects.select_related('user').get(id=int(id), user=user)
+
 
     def resolve_todos(root, info):
 
         user = info.context.user
         return Todo.objects.filter(user=user)
-    
+
 
 
 schema = graphene.Schema(query=Query)
